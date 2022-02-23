06cd1be7b7f20ed4ba5a8327ca152ae07d204844
Ben Merchant <benjaminmerchant@gmail.com>
2022-02-22 23:24:30 -0500

    close enough for government work
    lets publish it

--- src/App.js
+++ src/App.js
@@ -5,14 +5,22 @@ import Posts from './components/Posts'
 
 class App extends Component {
   state = {
-
+    posts: [
+      { id: 1001, title: 'REACTionary', body: 'Respond rather than React.'},
+      { id: 1002, title: 'ANGULAR Momentum', body: 'Forward acceleration over instantaneous velocity.'},
+      { id: 1003, title: 'Change My VUE', body: 'Attraction not promotion.'},
+      { id: 1004, title: 'From the EMBERs', body: 'Rise from the ashes anew.'},
+      { id: 1005, title: 'You\'re Looking SVELTE', body: 'Be kind to yourself.'},
+    ]
   }
   render() {
     return (
       <div className='App'>
         <NavBar />
         <main className='container mt-5 pt-3'>
-          <Posts />
+          <Posts 
+            posts={this.state.posts}
+          />
         </main>
       </div>
     )
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
 class Posts extends Component {
   render() {
-    return 'postss';
+    const { posts } = this.props;
+    return (
+      <div>
+        {posts.map((post) => (
+          <div key={post.id} className='mb-2'>
+            <h2 className='blogTitle px-3'>{post.title}</h2>
+            {post.body}
+          </div>
+        ))}
+      </div>
+    );
   }
 }
 
